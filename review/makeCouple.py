a0f74d602d6ba4ac75d440b6805c7d70bc157c04
jngcii <concotree@gmail.com>
2019-08-22 15:29:18 +0900

    Review : make couple

---
+++ review/makeCouple.py
@@ -0,0 +1,12 @@
+def makeCouple(a):
+
+    for i in range(0, len(a)-1):
+
+        for j in range(i+1, len(a)):
+
+            print(a[i], " --- ", a[j]);
+
+
+arr = ["jenny", "jisu", "jngcii", "nxxc", "nico"]
+
+makeCouple(arr)
