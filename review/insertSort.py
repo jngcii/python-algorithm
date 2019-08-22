9fadd20b9e56119dee560fefc1550c4c1bb8d774
jngcii <concotree@gmail.com>
2019-08-22 16:57:09 +0900

    Review : insertSort

---
+++ review/insertSort.py
@@ -0,0 +1,25 @@
+def insertSort(arr):
+
+    for i in range(1, len(arr)):
+
+        key=arr[i]
+        
+        j = i-1
+
+        while(j>=0 and arr[j] > key):
+
+            arr[j+1] = arr[j]
+
+            j -= 1
+
+        arr[j+1] = key
+
+
+
+
+
+arr = [6,2,4,9,8]
+
+insertSort(arr)
+
+print(arr)
